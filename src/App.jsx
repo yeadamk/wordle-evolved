1fbcc7e94b1d37d1b3bf28ae2ce6f8ef7ee79f01
Nikolas Rodriguez <niko23@g.ucla.edu>
2024-02-26 22:18:21 -0800

    Added a button that will be used to start the daily game. Does not actually start the game yet, as game functionality has yet to be coded. Instead, displays a message that the button was clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Click To Start Daily Game!");
 
   return (
     <>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Wordle Evolved</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setMessage("Button Clicked! Daily Game Starting Now!")}> {message}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
