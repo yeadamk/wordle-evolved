f474e964884584ba4b79bdd71b2b66da7abe13f5
Leo Thit <gthit@g.ucla.edu>
2024-02-29 12:25:46 -0800

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import Auth from './Auth';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GamePlay from './GamePlay';
 
 function App() {
   return (
-    <>
-      <Auth />
-      <GamePlay />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/gameplay' element={<GamePlay />} />
+      </Routes>
+    </Router>
   );
 }
 
