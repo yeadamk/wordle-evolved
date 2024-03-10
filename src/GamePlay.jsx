5df3b95f1bca893347297d86d54c828f4978d766
Dawg <pavan.gudavalli1@gmail.com>
2024-03-09 23:55:23 -0800

    No change

--- src/GamePlay.jsx
+++ src/GamePlay.jsx
@@ -343,7 +343,10 @@ function GamePlay({ userId, userName, setIsSignedIn }) {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div> */}
-
+      <Header 
+        userId={userId}
+        userName={userName}
+      />
       {userId ? (
         <>
           <h1>Welcome {userName}!!!</h1>
